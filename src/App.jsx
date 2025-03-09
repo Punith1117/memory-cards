6704b936efa63e4c27b9f90f5a70e515ded313ba
Punith1117 <punith11172006@gmail.com>
2025-03-09 17:15:37 +0530

    add basic implementation of fetching data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Cards from './components/Cards'
 
-function App() {
-  const [count, setCount] = useState(0)
+let castArray = []
+let fetchedArray = []
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+function App() { 
+  const [fetched, setFetched] = useState(false)
+
+  useEffect(() => {
+    if (fetched == false) {
+      fetch('https://api.tvmaze.com/shows/17861/cast')
+      .then(result => result.json())
+      .then(result => {
+        fetchedArray = result
+        setFetched(true)
+      })
+    }
+  }, [])
+
+  if (fetched == false) {
+    console.log('loading')
+  } else {
+    if (castArray.length == 0) {
+      fetchedArray.slice(0, 12).map(item => {
+        castArray.push({
+          id: item.character.id,
+          name: item.character.name,
+          imageUrl: item.character.image.medium
+        })
+      })
+    }
+  }
 }
 
 export default App
