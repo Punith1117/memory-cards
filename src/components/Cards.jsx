465e81415b452510852ef89e468c8076014b1e04
Punith1117 <punith11172006@gmail.com>
2025-03-09 18:00:21 +0530

    wrap the figure in a button to make it semantically clickable

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,10 +2,12 @@ export default function Cards({castArray}) {
     return (
         <section className="cards">
             {castArray.map((item) => (
-                <figure key={item.id}>
-                    <img src={item.imageUrl} alt="some-photo" />
-                    <figcaption>{item.name}</figcaption>
-                </figure>
+                <button key={item.id}>
+                    <figure>
+                        <img src={item.imageUrl} alt="some-photo" />
+                        <figcaption>{item.name}</figcaption>
+                    </figure>
+                </button>
             ))}
         </section>
     )
